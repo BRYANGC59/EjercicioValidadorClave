720aa1002cfa0f85a54efe5ba02b903a89b67e12
Bryan <bryangarciacastano@gmail.com>
2025-05-10 18:32:39 -0500

    abstract class ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,7 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada: int = longitud_esperada
+
